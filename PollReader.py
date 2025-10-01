d90cef5ea0566ee87a44d739af1655e5fe7927c9
mhueng <mhueng@umich.edu>
2025-10-01 13:49:23 -0400

    likelyvoterpush

--- PollReader.py
+++ PollReader.py
@@ -99,7 +99,18 @@ class PollReader():
             tuple: A tuple containing the average polling percentages for Harris and Trump
                    among likely voters, in that order.
         """
-        pass
+        harris_lv = []
+        trump_lv = []
+        
+        for i in range(len(self.data_dict['sample type'])):
+            if self.data_dict['sample type'][i] == 'LV':
+                harris_lv.append(self.data_dict['Harris result'][i])
+                trump_lv.append(self.data_dict['Trump result'][i])
+                
+        harris_avg = sum(harris_lv)/len(harris_lv)
+        trump_avg = sum(trump_lv)/len(trump_lv)
+        
+        return (harris_avg, trump_avg)
 
 
     def polling_history_change(self):
